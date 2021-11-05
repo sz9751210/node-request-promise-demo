c98c85f438fcf8b6fe9debb897f21e6e2ae042e7
alan wang <sz9751210@gmail.com>
2021-11-05 10:28:09 +0800

    add sort status

---
+++ sort_status.sh
@@ -0,0 +1,75 @@
+#!/bin/bash +x
+
+
+while :; do
+
+    Keyword=($(ls ./logs/* | awk -F/ '{print $3}' | awk -F. '{print $1}' | uniq))
+
+    echo -en "請輸入要指定的檔案關鍵字 :\n"
+    echo "退出請按q"
+    for key in ${!Keyword[@]}; do
+        echo "$key ${Keyword[$key]}"
+    done
+
+    read keyword_num
+    if [ $keyword_num == "q" ]; then
+        echo "退出"
+        exit
+    fi
+
+    keyword=${Keyword[keyword_num]}
+    # 列出檔案
+    Files=($(ls -td ./logs/*| grep $keyword))
+
+    # 輸入檔案編號
+    echo -en "請輸入要查看的檔案 :\n"
+    echo "退出請按q"
+    for key in ${!Files[@]}; do
+        echo "$key ${Files[$key]:7}"
+    done
+
+    read file_num
+    if [ $file_num == "q" ]; then
+        echo "退出"
+        exit
+    fi
+
+    file=${Files[file_num]}
+
+    # 確認是否為壓縮檔
+    if [[ $file =~ gz.*$ ]]; then
+        cat="gunzip -c"
+    else
+        cat="cat"
+    fi
+
+    # 輸入日期
+    #echo '此份log日期'
+    #echo $(awk  'BEGIN{OFS=" ";}{print $1}' $file |uniq )
+    total=`$cat $file |wc -l | awk '{print $1}'`
+    echo -en "請輸入要查看的日期以及時間(如需要全列出請按enter) :\n"
+    # 時間判斷
+    read timestamp
+    if  [ "$timestamp" == "q" ]; then
+        echo "退出"
+        exit
+    fi
+
+    echo '---------------------------------- status Code 狀態 ----------------------------------'
+    if [ -n "$timestamp" ]; then
+    vals=($total $timestamp)
+    $cat $file | sort  | awk -f log_status_anal.awk -vvals="${vals[*]}"
+    else
+    vals=($total)
+    $cat $file | sort  |  awk -f log_status_anal.awk -vvals="${vals[*]}"
+    fi
+    echo -e
+    echo -en "是否繼續? :\n"
+    echo "繼續請按enter，退出請按q"
+
+    read process
+    if [ "$process" == "q" ]; then
+        echo "退出"
+        exit
+    fi
+done
